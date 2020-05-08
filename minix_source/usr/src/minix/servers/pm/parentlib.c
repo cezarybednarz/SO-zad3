1eab4c86f337916a520cfc5c7e60e53c422d8715
cezarybednarz <cezary.bednarz@gmail.com>
2020-05-08 17:44:19 +0200

    przed testami

--- minix_source/usr/src/minix/servers/pm/parentlib.c
+++ minix_source/usr/src/minix/servers/pm/parentlib.c
@@ -3,19 +3,14 @@
 #include "mproc.h"
 
 int do_changeparent(void) {
-	printf("changeparent\n");
 	if(mp->mp_parent == INIT_PROC_NR) {
-		printf("zwracam EACCES\n");
 		return(EACCES);
 	}
 
 	if(mproc[mp->mp_parent].mp_flags & WAITING) {
-		printf("zwaracam EINVAL\n");
 		return(EINVAL);
 	}
 
-
-	printf("zmieniam parenta [%d] z %d na %d\n", mp->mp_pid,  mproc[mp->mp_parent].mp_pid, mproc[mproc[mp->mp_parent].mp_parent].mp_pid);
 
 	mp->mp_parent = mproc[mp->mp_parent].mp_parent;
 
@@ -24,7 +19,6 @@ int do_changeparent(void) {
 
 
 int do_getoppid(void) {
-	printf("getoppid\n");
 	pid_t pid = m_in.m1_i1;
 	register struct mproc *rp;
 	int found = 0;
@@ -36,11 +30,8 @@ int do_getoppid(void) {
 	}
 
 	if(!found || (rp->mp_first_parent == RS_NOT_INIT_PID)) {
-		printf("zwracam EINVAL\n");
 		return(EINVAL);
 	}
-
-	printf("pid rodzica to %d  xd\n", rp->mp_first_parent);
 
 	m_in.m1_i1 = rp->mp_first_parent;
 
